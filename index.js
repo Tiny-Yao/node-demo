5dec15df913ec73f6fe707886d2c7095546bae66
向日葵 <xiaoli.yao@jaderd.com>
2023-06-06 14:47:02 +0800

    🎨 perf:

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require('node:http')
 
-const hostname = '127.0.0.1'
+const hostname = '43.133.8.219'
 const port = 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello,World!\n')
+  res.end('你好,世界!\n')
 })
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`服务器开始运行 http://${hostname}:${port}/`)
 })
